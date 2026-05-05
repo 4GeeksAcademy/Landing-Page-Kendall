b99701fe0b03d9d7507ea6091ed0188d7c7d282c
Kenube21 <kendallsh51@gmail.com>
2026-05-05 01:55:32 +0000

    details

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="bg-dark text-white py-4 mt-4">
+      <div className="container text-center">
+        <p className="mb-0">Copyright © Your Website 2026</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,20 +1,47 @@
 import React from "react";
 
 
-import { Navbar, Jumbotron, Card, Footer } from "./content";
+import Navbar from "./Navbar.jsx";
+import Jumbotron from "./Jumbotron.jsx";
+import Card from "./Cards.jsx";
+import Footer from "./Footer.jsx";
 
 //create your first component
 const Home = () => {
+	const cardImages = [
+		{
+			url: "https://images.unsplash.com/photo-1772289935157-f64bc178b47d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D",
+			title: "Grand Canon"
+		},
+        {
+			url: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyMnx8fGVufDB8fHx8fA%3D%3D",
+			title: "River"
+		},
+        {
+			url: "https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
+			title: "Image 3"
+		},
+        {
+			url: "https://images.unsplash.com/photo-1505678261036-a3fcc5e884ee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D",
+			title: "Image 4"
+		},
+		{
+			url: "https://images.unsplash.com/photo-1777195680745-9fb67291b216?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMXx8fGVufDB8fHx8fA%3D%3D",
+			title: "Image 4"
+		}
+	];
+        
 	return (
 		<div className="d-flex flex-column min-vh-100">
 			<Navbar/>
 			<div className="container-fluid d-flex flex-column justify-content-center gap-4 mt-4">
 				<Jumbotron/>
 				<div className="container-fluid d-flex flex-wrap justify-content-center gap-4">
-					<Card imageId={1}/>
-					<Card imageId={2}/>
-					<Card imageId={3}/>
-					<Card imageId={4}/>
+					{
+						cardImages.map((img,index) => {
+							return <Card key={index} url={img.url} title={img.title} />
+						})
+					}
 				</div>
 			</div>
 			<Footer/>
